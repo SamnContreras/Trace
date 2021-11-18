e6a3e2e4c4e5a7bc15d17eccf152d7d760fb37b2
Samuel Conteras <sammycontreras7@gmail.com>
2021-11-18 15:53:44 -0700

    test11

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("/rollback", (req, res) => {
     rollbar.info("HTML file served successfully");
+    console.log("error")
 });
-
 
 app.use(rollbar.errorHandler());
