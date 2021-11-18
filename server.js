a118e2708cc84ccdb8fe07b33c693eb287f29f82
Samuel Conteras <sammycontreras7@gmail.com>
2021-11-18 13:56:27 -0700

    setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '795beb5ad25240629b875479e89f02f0',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.use(rollbar.errorHandler());
